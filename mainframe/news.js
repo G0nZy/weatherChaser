0f536176ccafc955a568e9fa0915feb68efaaa06
G0nZy <knicksfan1297@gmail.com>
2020-06-17 14:39:34 -0400

    forms

---
+++ mainframe/news.js
@@ -0,0 +1,70 @@
+// api key 7f59dcc3c46e407096062acd4a0f046d
+
+
+
+
+// var url = 'http://newsapi.org/v2/top-headlines?country=us&category=business&api' +
+// 'country=us&' +
+// 'apiKey=7f59dcc3c46e407096062acd4a0f046d';
+// var req = new Request(url);
+// fetch(req)
+// .then(function(response) {
+// console.log(response.json());
+// })
+
+
+
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('7f59dcc3c46e407096062acd4a0f046d');
+// To query /v2/top-headlines
+// All options passed to topHeadlines are optional, but you need to include at least one of them
+newsapi.v2.topHeadlines({
+  sources: 'bbc-news,the-verge',
+  q: 'bitcoin',
+  category: 'business',
+  language: 'en',
+  country: 'us'
+}).then(response => {
+  console.log(response);
+  /*
+    {
+      status: "ok",
+      articles: [...]
+    }
+  */
+});
+// To query /v2/everything
+// You must include at least one q, source, or domain
+newsapi.v2.everything({
+  q: 'bitcoin',
+  sources: 'bbc-news,the-verge',
+  domains: 'bbc.co.uk, techcrunch.com',
+  from: '2017-12-01',
+  to: '2017-12-12',
+  language: 'en',
+  sortBy: 'relevancy',
+  page: 2
+}).then(response => {
+  console.log(response);
+  /*
+    {
+      status: "ok",
+      articles: [...]
+    }
+  */
+});
+// To query sources
+// All options are optional
+newsapi.v2.sources({
+  category: 'technology',
+  language: 'en',
+  country: 'us'
+}).then(response => {
+  console.log(response);
+  /*
+    {
+      status: "ok",
+      sources: [...]
+    }
+  */
+});
